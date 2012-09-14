399d37b30129a9ab6d95f129a70ead45a7d73240
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-09-14 10:27:19 +0200

    canvas.go

---
+++ canvas.go
@@ -0,0 +1,79 @@
+package bitbuilder
+
+// Author: Arne Vansteenkiste
+// Some code snippets were taken from 
+// https://code.google.com/p/appengine-go/
+
+import (
+	"code.google.com/p/freetype-go/freetype/raster"
+	"fmt"
+	"image"
+	"image/color"
+	"image/png"
+	"os"
+)
+
+type Canvas struct{
+	*image.RGBA
+	painter *raster.RGBAPainter
+	rasterizer *raster.Rasterizer
+	strokewidth raster.Fix32
+	path raster.Path
+}
+
+func NewCanvas(w, h int) *Canvas{
+	c := new(Canvas)
+	c.RGBA = image.NewRGBA(image.Rect(0, 0, w, h))
+	c.painter = raster.NewRGBAPainter(c.RGBA)
+	c.rasterizer = raster.NewRasterizer(w, h)
+	c.rasterizer.UseNonZeroWinding = true
+	c.SetColor(color.Black)
+	c.path = make(raster.Path, 0, 100)
+	return c
+}
+
+func(c*Canvas)SetColor(col color.Color){
+	c.painter.SetColor(col)
+}
+
+func(c*Canvas)SetStroke(width int){
+	c.strokewidth = fix32(width)
+}
+
+func(c*Canvas) Line(x1, y1, x2, y2 int){
+	c.resetPath()
+	c.path.Start(pt(x1, y1))
+	c.path.Add1(pt(x2, y2))
+	c.renderPath()
+}
+
+func(c*Canvas)resetPath(){
+	c.path = c.path[:0]
+}
+
+func(c*Canvas)renderPath(){
+	c.rasterizer.Rasterize(c.painter)
+}
+
+func (c*Canvas)Encode(fname string) {
+	out, err := os.OpenFile(fname, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0755)
+	check("topng", err)
+	defer out.Close()
+	err = png.Encode(out, c.RGBA)
+	check("topng", err)
+}
+
+func check(msg string, err error) {
+	if err != nil {
+		fmt.Fprintln(os.Stderr, msg, ":", err)
+		os.Exit(1)
+	}
+}
+
+func pt(x, y int) raster.Point {
+	return raster.Point{fix32(x), fix32(y)}
+}
+
+func fix32(x int) raster.Fix32 {
+	return raster.Fix32(x << 8)
+}
